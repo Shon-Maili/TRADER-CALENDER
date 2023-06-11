e07fb182c4941636617c0034289fff08e82fe52a
Shon-Maili <125014199+Shon-Maili@users.noreply.github.com>
2023-06-11 15:10:21 +0300

    avatar - choosing file

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,15 +1,24 @@
 import "./About.css"
 import React from "react"
-
+import UploadAvatar from "./avatar/UploadAvatar";
 
 export default function About(props) {
-
-
-
+    
+    function handleFileChange(event) {
+      const selectedFile = event.target.files[0];
+      // Handle the selected file here
+      console.log('File selected:', selectedFile);
+     
+    };
+  
     return(
         <div id="AboutPage">
         <form className="form" onSubmit={props.handleFormSubmit}>
+        <label htmlFor="fileInput" id="customFileLabel">
             <img className="avatarLogo" src="./images/avatarPage.png" alt=""/>
+            </label>
+            <input  type="file" id="fileInput" style={{ display: 'none' }} onChange={handleFileChange} />
+            
             <div className="inputsFields">
             <div className="group">
             
@@ -26,4 +35,4 @@ export default function About(props) {
         </div>
     )
        
-}
+    }
